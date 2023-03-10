02541b74efa2a0d1e65230ba7823b8471e22ad91
Boyan <92653208+BoyanK95@users.noreply.github.com>
2023-03-10 20:58:22 +0200

    Conected powerService to DiskModule

--- src/power/power.service.ts
+++ src/power/power.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PowerService {
-  SupplyPower(watss: number) {
+  supplyPower(watss: number) {
     console.log(`Supplying ${watss} watss worth of power.`);
   }
 }
